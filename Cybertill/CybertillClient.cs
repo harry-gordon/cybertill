e0ff82b0f3cb77cf20bb107e38610879680e9ddb
Harry Gordon <harrygordon1000@gmail.com>
2020-12-07 11:52:45 +0000

    Moved most of the code inside a CybertillClient that can be used to make calls (still exposes the internal client but it handles auth)

---
+++ Cybertill/CybertillClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using Cybertill.Console;
+using Cybertill.Soap;
+
+namespace Cybertill
+{
+    public class CybertillClient : ICybertillClient
+    {
+        private readonly CybertillConfig _config;
+        private readonly CybertillApi_v1_6PortTypeClient _client;
+        private string _authValue;
+
+        public CybertillClient(CybertillConfig config)
+        {
+            _config = config;
+
+            var timeout = TimeSpan.FromSeconds(120);
+
+            _client =
+                new CybertillApi_v1_6PortTypeClient(
+                    _config.EndpointUrl,
+                    timeout
+                );
+        }
+
+        public async Task InitAsync()
+        {
+            _authValue = await _client.authenticate_getAsync(_config.Username, _config.AuthId);
+        }
+
+        public Task<T> ExecuteAsync<T>(Func<CybertillApi_v1_6PortTypeClient, Task<T>> func)
+        {
+            CheckAuth();
+
+            using (var scope = new OperationContextScope(_client.InnerChannel))
+            {
+                // Add SOAP Header to an outgoing request
+                var header = MessageHeader.CreateHeader("Authentication", string.Empty, $"Basic {_authValue}");
+                OperationContext.Current.OutgoingMessageHeaders.Add(header);
+
+                /*
+                // Add HTTP Header to an outgoing request
+                var requestMessage = new HttpRequestMessageProperty();
+                requestMessage.Headers["Authentication"] = $"Basic {authResult}";
+                OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name]
+                    = requestMessage;
+                */
+
+                return func(_client);
+            }
+        }
+
+        private void CheckAuth()
+        {
+            if (_authValue == null)
+            {
+                throw new InvalidOperationException("Client is not authorized, please initialise the client before making an calls");
+            }
+        }
+    }
+}
---
+++ Cybertill/ICybertillClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cybertill.Soap;
+
+namespace Cybertill
+{
+    public interface ICybertillClient
+    {
+        Task InitAsync();
+        Task<T> ExecuteAsync<T>(Func<CybertillApi_v1_6PortTypeClient, Task<T>> func);
+    }
+}
